c3167e44b136fbfdd83c4262ebc578e8e49320d9
YuanSyun <rty3497374@gmail.com>
2018-12-26 15:37:18 +0800

    [New] Recording the hand action

---
+++ Assets/BrainBoardingVR/Scripts/GameobjectRecorder.cs
@@ -0,0 +1,54 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEditor.Experimental.Animations;
+
+public class GameobjectRecorder : MonoBehaviour {
+
+	public AnimationClip clip;
+
+	public bool record = false;
+
+	private GameObjectRecorder _MyRecorder;
+
+	// Use this for initialization
+	void Start () {
+		
+
+		_MyRecorder = new GameObjectRecorder();
+		_MyRecorder.root = gameObject;
+
+		_MyRecorder.BindComponent<Transform>(gameObject, true);
+
+		Debug.Log("record gameobject name: " + gameObject.name);
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		
+		if(clip == null)
+			return;
+
+		if(record){
+			Debug.Log("start record");
+			_MyRecorder.TakeSnapshot(Time.deltaTime);
+		}else{
+			if(_MyRecorder.isRecording){
+				Debug.Log("end record");
+				_MyRecorder.SaveToClip(clip);
+				_MyRecorder.ResetRecording();
+			}
+		}
+
+	}
+
+	public void StartRecording(){
+		record = true;
+	}
+
+	public void StopRecording(){
+		record = false;
+	}
+}
---
+++ Assets/BrainBoardingVR/Scripts/SteamVRControllerButtonEvents.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.Events;
+
+
+public class SteamVRControllerButtonEvents : MonoBehaviour {
+
+	public UnityEvent TriggerClicked;
+	public UnityEvent TrggerUnclicked;
+	
+	private SteamVR_TrackedController _controller;
+
+	// Use this for initialization
+	void Start () {
+		
+		_controller = GetComponent<SteamVR_TrackedController>();
+
+		if (_controller == null) {
+			Debug.Log ("not found steamvr tracked controller");
+		}
+
+		_controller.TriggerClicked += HandleTriggerClicked;
+		_controller.TriggerUnclicked += HandleTriggerUnclicked;
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	private void HandleTriggerClicked(object sender, ClickedEventArgs e){
+		TriggerClicked.Invoke ();
+	}
+
+	private void HandleTriggerUnclicked(object sender, ClickedEventArgs e){
+		TrggerUnclicked.Invoke ();
+	}
+
+
+		
+}
